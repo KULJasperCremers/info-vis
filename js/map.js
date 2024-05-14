a54b4a37b5bf7535af100d022763710bb95375e9
Stijn <48821681+Stijn-Hendrix@users.noreply.github.com>
2024-05-14 22:43:27 +0200

    highlighting

--- js/map.js
+++ js/map.js
@@ -19,7 +19,7 @@ let filteredFeatures;
 let europeProjection;
 let pathGenerator;
 
-function colorMap(htmlId, colorFunc) {
+function colorMap(htmlId, colorFunc, showGrowthGradient = false) {
     let svg = d3.select(htmlId).select("svg");  // Select the existing SVG by HTML ID
 
     var colorFuncOutputs = {}
@@ -27,6 +27,8 @@ function colorMap(htmlId, colorFunc) {
     for (let key in countryCodes) {
         colorFuncOutputs[key] = colorFunc(key);
     }
+
+    console.log("t")
 
     svg.selectAll("path")
        .attr("fill", function(d) {
@@ -52,7 +54,6 @@ function colorMap(htmlId, colorFunc) {
     .enter()
     .append("text")
     .attr("transform", function(d) { 
-        console.log(d);
         const centroid = pathGenerator.centroid(d);
         return `translate(${centroid[0]}, ${centroid[1]})`;
     })
@@ -68,42 +69,49 @@ function colorMap(htmlId, colorFunc) {
     .style("font-size", "10px") // Adjust size as needed
     .style("pointer-events", "none"); // Prevents the label from interfering with mouse events on the map
 
-    var defs = svg.append("defs");
-    var linearGradient = defs.append("linearGradient")
-        .attr("id", "linear-gradient")
-        .attr("x1", "0%")
-        .attr("y1", "100%")  // Start at the bottom
-        .attr("x2", "0%")
-        .attr("y2", "0%");   // End at the top
-    linearGradient.append("stop").attr("offset", "0%").attr("stop-color", "red");  // Color starts at the bottom
-    linearGradient.append("stop").attr("offset", "100%").attr("stop-color", "green");  // Color ends at the top
+    svg.selectAll("defs").remove();
+
+    if (showGrowthGradient) {
+
+        var defs = svg.append("defs");
+        var linearGradient = defs.append("linearGradient")
+            .attr("id", "linear-gradient")
+            .attr("x1", "0%")
+            .attr("y1", "100%")  // Start at the bottom
+            .attr("x2", "0%")
+            .attr("y2", "0%");   // End at the top
+        linearGradient.append("stop").attr("offset", "0%").attr("stop-color", "red");  // Color starts at the bottom
+        linearGradient.append("stop").attr("offset", "100%").attr("stop-color", "green");  // Color ends at the top
 
 
-    // Append and position the rectangle, and use raise to ensure it's on top
-    svg.append("rect")
-        .attr("x", 10)
-        .attr("y", 20)
-        .attr("width", 20)
-        .attr("height", 100)
-        .style("fill", "url(#linear-gradient)")
-        .raise(); // Ensure the rectangle is on top
+        // Append and position the rectangle, and use raise to ensure it's on top
+        svg.append("rect")
+            .attr("x", 10)
+            .attr("y", 20)
+            .attr("width", 20)
+            .attr("height", 100)
+            .style("fill", "url(#linear-gradient)")
+            .raise(); // Ensure the rectangle is on top
 
+            svg.append("text")
+            .attr("x", 10 + 10)  // x position, center of the rectangle
+            .attr("y", 15)       // y position, slightly above the rectangle
+            .attr("text-anchor", "middle") // Center the text horizontally
+            .style("font-size", "12px")
+            .style("font-family", "Arial, sans-serif")
+            .text("Growth");
+        
+        // Append text for "Decline" at the bottom of the rectangle
         svg.append("text")
-        .attr("x", 10 + 10)  // x position, center of the rectangle
-        .attr("y", 15)       // y position, slightly above the rectangle
-        .attr("text-anchor", "middle") // Center the text horizontally
-        .style("font-size", "12px")
-        .style("font-family", "Arial, sans-serif")
-        .text("Growth");
-    
-    // Append text for "Decline" at the bottom of the rectangle
-    svg.append("text")
-        .attr("x", 10 + 10)  // x position, center of the rectangle
-        .attr("y", 20 + 100 + 10) // y position, slightly below the rectangle
-        .attr("text-anchor", "middle") // Center the text horizontally
-        .style("font-size", "12px")
-        .style("font-family", "Arial, sans-serif")
-        .text("Decline");
+            .attr("x", 10 + 10)  // x position, center of the rectangle
+            .attr("y", 20 + 100 + 10) // y position, slightly below the rectangle
+            .attr("text-anchor", "middle") // Center the text horizontally
+            .style("font-size", "12px")
+            .style("font-family", "Arial, sans-serif")
+            .text("Decline");
+
+    }
+
 }
 
 function AppendMap(htmlId, onHoverCountryFunc, onClickCountryFunc, colorFunc = undefined, width = 900, height = 500) {
@@ -131,16 +139,11 @@ function AppendMap(htmlId, onHoverCountryFunc, onClickCountryFunc, colorFunc = u
         .attr("d", pathGenerator)
         .attr("stroke", function(d) { return d.properties.name !== "Iceland" ? "grey" : "rgba(211, 211, 211, 0)" })
         .attr("fill", function(d) {
-            var countryExists = countryCodes.hasOwnProperty(d.properties.name);
-            if (colorFunc !== undefined && countryExists) {
-                return colorFunc(d.properties.name)[1];
-            }
-            else {
+                var countryExists = countryCodes.hasOwnProperty(d.properties.name);
                 if (d.properties.name == "Iceland") {
                     return  "rgba(211, 211, 211, 0)";
                 }
                 return countryExists ? "white" : "rgba(211, 211, 211, 1)";
-            }
         })
         .each(function(d) {
             if (countryCodes.hasOwnProperty(d.properties.name)) {
@@ -168,7 +171,6 @@ function AppendMap(htmlId, onHoverCountryFunc, onClickCountryFunc, colorFunc = u
             .enter()
             .append("text")
             .attr("transform", function(d) { 
-                console.log(d);
                 const centroid = pathGenerator.centroid(d);
                 return `translate(${centroid[0]}, ${centroid[1]})`;
             })
@@ -177,7 +179,7 @@ function AppendMap(htmlId, onHoverCountryFunc, onClickCountryFunc, colorFunc = u
             .attr("fill", "black") // You can change the color based on conditions or keep it fixed
             .text(function(d) {
                 var countryExists = countryCodes.hasOwnProperty(d.properties.name);
-                if (colorFunc !== undefined && countryExists) {
+                if (countryExists) {
                     return "";
                 }
             })
@@ -185,44 +187,6 @@ function AppendMap(htmlId, onHoverCountryFunc, onClickCountryFunc, colorFunc = u
             .style("pointer-events", "none"); // Prevents the label from interfering with mouse events on the map
         });
     });
-
-
-    var defs = svg.append("defs");
-    var linearGradient = defs.append("linearGradient")
-        .attr("id", "linear-gradient")
-        .attr("x1", "0%")
-        .attr("y1", "100%")  // Start at the bottom
-        .attr("x2", "0%")
-        .attr("y2", "0%");   // End at the top
-    linearGradient.append("stop").attr("offset", "0%").attr("stop-color", "red");  // Color starts at the bottom
-    linearGradient.append("stop").attr("offset", "100%").attr("stop-color", "green");  // Color ends at the top
-
-
-    // Append and position the rectangle, and use raise to ensure it's on top
-    svg.append("rect")
-        .attr("x", 10)
-        .attr("y", 20)
-        .attr("width", 20)
-        .attr("height", 100)
-        .style("fill", "url(#linear-gradient)")
-        .raise(); // Ensure the rectangle is on top
-
-        svg.append("text")
-        .attr("x", 10 + 10)  // x position, center of the rectangle
-        .attr("y", 15)       // y position, slightly above the rectangle
-        .attr("text-anchor", "middle") // Center the text horizontally
-        .style("font-size", "12px")
-        .style("font-family", "Arial, sans-serif")
-        .text("Growth");
-    
-    // Append text for "Decline" at the bottom of the rectangle
-    svg.append("text")
-        .attr("x", 10 + 10)  // x position, center of the rectangle
-        .attr("y", 20 + 100 + 10) // y position, slightly below the rectangle
-        .attr("text-anchor", "middle") // Center the text horizontally
-        .style("font-size", "12px")
-        .style("font-family", "Arial, sans-serif")
-        .text("Decline");
 
     return svg
 }
